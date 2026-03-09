f47a192d00ee4563f5aff1d03c9e755c38890419
Stefiinhaa <stefanisantos1212@gmail.com>
2026-03-08 23:43:33 -0300

    hihiha

--- conexao.php
+++ conexao.php
@@ -1,12 +1,13 @@
 <?php
-$host = "localhost";
-$usuario = "root";
-$senha = "";
-$banco = "oceancoffee";
+// Substitua pelos DADOS EXATOS que o InfinityFree lhe deu
+$servidor = "sql304.infinityfree.com"; // Substitua pelo seu Host Name
+$usuario = "if0_41337854";       // Substitua pelo seu User Name
+$senha = "STECLEri123 ";      // Substitua pela sua Password
+$banco = "if0_41337854_oceancoffee"; // Substitua pelo seu DB Name
 
-$conexao = new mysqli($host, $usuario, $senha, $banco);
+$conexao = new mysqli($servidor, $usuario, $senha, $banco);
 
 if ($conexao->connect_error) {
-    die("Falha na conexao");
+    die("Falha na conexao: " . $conexao->connect_error);
 }
 ?>
